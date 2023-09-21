1ee72065ba7d1be4864b5217c1133f9a3d1ec665
CodeSandbox <support@codesandbox.io>
2023-09-21 19:58:44 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import "./css/style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <p>Welcome to Vite</p>
-    </div>
+    <main className="App">
+      <h2>Prueba Calculadora Formulario</h2>
+      <div className="valores">
+        <label htmlFor="valor1">
+          Valor 1:
+          <input type="number" name="valor1" id="valor1" />
+        </label>
+        <label htmlFor="valor2">
+          Valor 2:
+          <input type="number" name="valor2" id="valor2" />
+        </label>
+      </div>
+      <div className="operaciones">
+        <button className="operacion" id="add">
+          +
+        </button>
+        <button className="operacion" id="substract">
+          -
+        </button>
+        <button className="operacion" id="division">
+          /
+        </button>
+        <button className="operacion" id="multiply">
+          *
+        </button>
+      </div>
+      <div className="final">
+        <button className="calcular" id="equals">
+          =
+        </button>
+      </div>
+      <section className="sec-resultado">
+        <p className="resultado">123456789</p>
+      </section>
+    </main>
   );
 }
 
