3f08666551f8ec8a5793125a718244ef09ee2434
Henry Cuesta <141352590+henry-cv@users.noreply.github.com>
2023-09-24 04:17:28 +0000

    aún falta párrafo resultado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-//import React from "react";
+import React from "react";
 import "./css/style.scss";
 
-const $valor1 = document.getElementById("valor1");
-const $valor2 = document.getElementById("valor2");
-const $result = document.getElementById("resultado");
+/* const $valor1 = document.getElementById("valor1");
+const $valor2 = document.getElementById("valor2"); */
+let $result = document.getElementById("resultado");
 
 let n1 = 0,
   n2 = 0;
-const validarValores = (val1 = 1, val2 = 3) => {
-  console.log(`tipo val1: ${typeof val1}, tipo val2: ${typeof val2}`);
-  n1 = parseFloat(val1);
-  n2 = parseFloat(val2);
+const validarValores = () => {
+  let $valor1 = document.getElementById("valor1");
+  let $valor2 = document.getElementById("valor2");  
+
+  console.log(`tipo val1: ${typeof $valor1.value}, tipo val2: ${typeof $valor2.value}`);
+  n1 = parseFloat($valor1.value);
+  n2 = parseFloat($valor2.value);
   if (typeof n1 !== "number" || typeof n2 !== "number") {
     $valor1.value = "";
     $valor2.value = "";
@@ -21,7 +24,8 @@ const restar = () => n1 - n2;
 const multiplicar = () => n1 * n2;
 const dividir = () => n1 / n2;
 
-function App() {
+function App() {  
+    
   const handleClick = (e) => {
     let oper = e.target.id;
     console.log(`elemento id: ${oper}`);
@@ -31,8 +35,8 @@ function App() {
       multiply: multiplicar(),
       divide: dividir(),
     };
-    validarValores($valor1.value, $valor2.value);
-    console.log(`valor 1: ${$valor1.value}, valor 2: ${$valor2.value}`);
+    validarValores();
+    //console.log(`valor 1: ${$valor1.value}, valor 2: ${$valor2.value}`);
     console.log(`número 1: ${n1}, número 2: ${n2}`);
     let result = operations[oper];
     console.log(`Resultado = ${result}`);
